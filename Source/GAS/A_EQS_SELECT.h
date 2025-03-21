f7ccc0be2fa77070f545cc51a08ec32936a76dab
Przemysław Pruszowski <przemyslaw.pruszowski@gmail.com>
2025-03-21 18:55:52 +0100

    Task 2 - code review 1 about project structure

--- Source/GAS/A_EQS_SELECT.h
+++ Source/GAS/A_EQS_SELECT.h
@@ -6,6 +6,12 @@
 #include "GameFramework/Actor.h"
 #include "A_EQS_SELECT.generated.h"
 
+// Code review: Naming should be consistent with other files and classes GAS...
+// some folder structure should be introduced for c++ files
+// blueprint names are also inconsistent like endlessWorldPlayerController - this file is in camel case instead of pascal
+// we have multiple ThirdPerson folders and it variations, 
+
+
 UCLASS()
 class GAS_API A_EQS_SELECT : public AActor
 {
